e0a452aa82c9703709e0556b0c2302226080774c
Blake Williamson <miya.ashbrook@gmail.com>
2023-04-21 18:21:43 -0700

    fix for new blender version

--- __init__.py
+++ __init__.py
@@ -1,58 +1,123 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+
 bl_info = {
-	"name": "Yuk2 format",
-	"author": "yukizini",
-	"version": (0, 0, 0),
-	"blender": (2, 74, 0),
-	"location": "File > Export, Scene properties",
-	"description": "Export yuk2",
-	"wiki_url": "http://github.com/yukizini",
-	"category": "Export",
+    "name": "yuk format",
+    "author": "yukizini",
+    "version": (2, 3, 1),
+    "blender": (2, 93, 0),
+    "location": "File > Import-Export",
+    "description": "Import-Export yuk",
+    "warning": "",
+    "doc_url": "https://www.github.com/yukitheclown",
+    "category": "Import-Export",
 }
 
 if "bpy" in locals():
-	import imp
-	if "export_yuk2" in locals():
-		imp.reload(export_yuk2)
+    if "export_yuk2" in locals():
+        importlib.reload(export_yuk2)
 
 import bpy
-from bpy.props import (BoolProperty)
-from bpy_extras.io_utils import ( ExportHelper, path_reference_mode, axis_conversion )
+from bpy.props import (
+        BoolProperty,
+        EnumProperty,
+        FloatProperty,
+        StringProperty,
+        )
+from bpy_extras.io_utils import (
+        ImportHelper,
+        ExportHelper,
+        orientation_helper,
+        axis_conversion,
+        path_reference_mode,
+        )
 
-class ExportYUK2(bpy.types.Operator, ExportHelper):
 
-	"""Save a yuk2 File"""
 
-	bl_idname = "export_scene.yuk2"
-	bl_label = 'Export yuk2'
-	bl_options = {'PRESET'}
+class YUK2Include(bpy.types.Panel):
+    bl_space_type = 'FILE_BROWSER'
+    bl_region_type = 'TOOL_PROPS'
+    bl_label = "Include"
+    bl_parent_id = "FILE_PT_operator"
 
-	filename_ext = ".yuk2"
+    @classmethod
+    def poll(cls, context):
+        sfile = context.space_data
+        operator = sfile.active_operator
 
-	exportAnim = BoolProperty(name="Export selected objects active animation?", default = True)
-	exportMesh = BoolProperty(name="Export selectced mesh?", default = True)
+        return operator.bl_idname == "EXPORT_SCENE_OT_yuk"
 
-	def execute(self, context):
+    def draw(self, context):
+        layout = self.layout
+        layout.use_property_split = True
+        layout.use_property_decorate = False  # No animation.
 
-		from . import export_yuk2
+        sfile = context.space_data
+        operator = sfile.active_operator
 
-		from mathutils import Matrix
-		keywords = self.as_keywords(ignore=("filename_ext", "check_existing" ))
+        layout.prop(operator, "exportMesh")
+        layout.prop(operator, "exportAnim")
 
-		keywords["globalMatrix"] = axis_conversion(to_forward='-Z', to_up='Y').to_4x4()
 
-		return export_yuk2.Export(self, context, **keywords)
+class Exportyuk(bpy.types.Operator, ExportHelper):
+    """Export selection to Extensible 3D file (.yuk)"""
+    bl_idname = "export_scene.yuk"
+    bl_label = 'Export yuk'
+    bl_options = {'PRESET'}
+
+    filename_ext = ".yuk"
+
+    exportMesh: BoolProperty(
+            name="Export mesh",
+            description="Export Mesh only",
+            default=False,
+            )
+    exportAnim: BoolProperty(
+            name="Export Animation",
+            description="Export current animation",
+            default=True,
+            )
+
+
+    def execute(self, context):
+        from . import export_yuk2
+
+        from mathutils import Matrix
+
+        keywords = self.as_keywords(ignore=("filename_ext", "check_existing" ))
+
+        keywords["globalMatrix"] = axis_conversion(to_forward='-Z', to_up='Y').to_4x4()
+
+        return export_yuk2.Export(self, context, **keywords)
+
+    def draw(self, context):
+        pass
+
+
 
 def menu_func_export(self, context):
-	self.layout.operator(ExportYUK2.bl_idname, "Export yuk2 (.yuk2)")
+    self.layout.operator(Exportyuk.bl_idname,
+                         text="yuk (.yuk)")
+
+
+classes = (
+    Exportyuk,
+    YUK2Include,
+)
+
 
 def register():
-	bpy.utils.register_module(__name__)
-	bpy.types.INFO_MT_file_export.append(menu_func_export)
+    for cls in classes:
+        bpy.utils.register_class(cls)
+
+    bpy.types.TOPBAR_MT_file_export.append(menu_func_export)
 
 
 def unregister():
-	bpy.utils.unregister_module(__name__)
-	bpy.types.INFO_MT_file_export.remove(menu_func_export)
+    bpy.types.TOPBAR_MT_file_export.remove(menu_func_export)
+
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
+
 
 if __name__ == "__main__":
-	register()
+    register()
